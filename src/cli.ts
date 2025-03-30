47f43ccf7a5e8f631908c1d3d8c888af0691fcf5
rubencito9294 <74841337+rubencito9294@users.noreply.github.com>
2025-03-30 18:08:43 -0500

    Feat: add transactions data analyzer

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,10 @@
 import { ParserCSV } from "./parcerCSV";
+import { TransactionDataAnalyzer } from "./transactionDataAnalyzer";
 
 const data = ParserCSV(process.argv[2]);
 console.log("Ruben: data", data);
+const analizer = new TransactionDataAnalyzer(data);
+
+console.log("Ruben: analizer", analizer.CalculateFinalBalance());
+console.log("Ruben: analizer", analizer.findHighestTransaction());
+console.log("Ruben: analizer", analizer.countTransactionsByType());
