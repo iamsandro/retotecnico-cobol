0d2d426c579bd8192520220f917a6a4b3da864ec
rubencito9294 <74841337+rubencito9294@users.noreply.github.com>
2025-03-29 19:04:58 -0500

    Folder and file structure creation

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Exporta las clases principales para que otros proyectos las usen
+export { ParserCSV } from "./parcerCSV";
+export { TransactionDataAnalyzer } from "./transactionDataAnalyzer";
+export { ReportGenerator } from "./reportGenerator";
+export type { Transaction } from "./types";
