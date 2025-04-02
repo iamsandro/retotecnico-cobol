004b3078ff73802728fdf4b66c632d68cf9ceee8
rubencito9294 <74841337+rubencito9294@users.noreply.github.com>
2025-04-01 22:33:13 -0500

    add types

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,9 @@
  * Transacción bancaria
  */
 export interface Transaction {
-    id: string;
-    tipo: "Crédito" | "Débito";
-    monto: number;
+  id: string;
+  tipo: "Crédito" | "Débito";
+  monto: number;
 }
 
 /**
@@ -12,6 +12,48 @@ export interface Transaction {
  * @property {number} Crédito - Número de transacciones de tipo "Crédito"
  */
 export interface TransactionCounts {
-    Crédito: number;
-    Débito: number;
+  Crédito: number;
+  Débito: number;
+}
+
+export interface DetailsPrompt {
+  type:
+    | "input"
+    | "number"
+    | "confirm"
+    | "list"
+    | "rawlist"
+    | "expand"
+    | "checkbox"
+    | "password"
+    | "editor";
+  name: string;
+  message: string | ((answers: Record<string, any>) => string);
+  default?:
+    | string
+    | number
+    | boolean
+    | Array<any>
+    | ((
+        answers: Record<string, any>,
+      ) => string | number | boolean | Array<any>);
+  choices?:
+    | Array<string | { name: string; value: any; short?: string }>
+    | ((
+        answers: Record<string, any>,
+      ) => Array<string | { name: string; value: any; short?: string }>);
+  validate?: (input: string) => boolean | string | Promise<boolean | string>;
+  filter?: (input: unknown, answers: Record<string, any>) => unknown;
+  transformer?: (
+    input: unknown,
+    answers: Record<string, any>,
+    flags: any,
+  ) => unknown;
+  when?: (answers: Record<string, any>) => boolean;
+  pageSize?: number;
+  prefix?: string;
+  suffix?: string;
+  askAnswered?: boolean;
+  loop?: boolean;
+  waitUserInput?: boolean;
 }
